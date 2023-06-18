760a9e3569bed2bbc8f81c97bad0abb14507fff2
NhtChung <nht.chung95@gmail.com>
2023-06-19 02:12:09 +0700

    uploadBT

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// import logo from './logo.svg';
+import BaiTapLayoutComponent from "./BaiTapLayoutComponent";
+
+function App() {
+  return <BaiTapLayoutComponent/>;
+}
+
+export default App;
---
+++ src/BaiTapLayoutComponent/index.js
@@ -0,0 +1,16 @@
+import Header from "./header";
+import Body from "./body";
+import Footer from "./footer";
+
+
+
+function BaiTapLayoutComponent(){
+    return (
+        <div>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    )
+}
+export default BaiTapLayoutComponent;
